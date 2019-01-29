4711c1147c78751f6c9849a462031189304ebe5f
Jack <jack.woodhouse.97@gmail.com>
2019-01-29 12:57:45 +0000

    Start

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { request } from "https";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) =>
+    res.send(`Node and Express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`your server is running on ${PORT}`)
+);
