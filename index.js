03b2a83c203fb133f129fea02abe4a7c6eb683fd
Jack <jack.woodhouse.97@gmail.com>
2019-01-30 13:04:15 +0000

    create mongoose connection in index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose';
+import bodyParser from 'body-parser';
 import { request } from "https";
 import routes from './source/routes/afrRoutes';
 
 const app = express();
 
 const PORT = 3000;
+
+//mongoose connection
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/AAFdb', {
+    useMongoClient: true
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 routes(app);
 
