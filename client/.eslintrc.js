6bd63f1988b11ff33ee61f83d8b5d785dc3b1428
Jack <jack.woodhouse.97@gmail.com>
2019-02-11 14:17:46 +0000

    Revert "Revert "n""
    This reverts commit 10f9a9fd965218ad91724fdf59f50d427419d49d.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    '@vue/airbnb'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "import/prefer-default-export": "off",
+    "indent": ["error", 4],
+    "no-console": "off"
   },
   parserOptions: {
-    parser: 'babel-eslint',
-  },
-};
+    parser: 'babel-eslint'
+  }
+}
