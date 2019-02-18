e9a82820fbf08157f2dfe7271c8215f0ad03b024
Jack <jack.woodhouse.97@gmail.com>
2019-02-18 19:58:07 +0000

    Revert "added array"
    This reverts commit 0988f23c2b2bff5135a747b1913c42fec465e781.

--- server/source/models/UserModel.js
+++ server/source/models/UserModel.js
@@ -11,31 +11,25 @@ export const UserSchema = new mongoose.Schema({
         type: String,
         required: 'enter a password',
     },
-    data:
-    [
-        {
-            firstName: {
-                type: String,
-                required: 'enter first name',
-            },
-            lastName: {
-                type: String,
-                required: 'enter last name',
-            },
-            email: {
-                type: String,
-                required: 'enter valid email',
-            },
-            phone: {
-                type: Number,
-            },
-            created_data: {
-                type: Date,
-                default: Date.now,
-            },
-
-        },
-    ],
+    firstName: {
+        type: String,
+        required: 'enter first name',
+    },
+    lastName: {
+        type: String,
+        required: 'enter last name',
+    },
+    email: {
+        type: String,
+        required: 'enter valid email',
+    },
+    phone: {
+        type: Number,
+    },
+    created_data: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 /* UserSchema.pre('save', (next) => {
